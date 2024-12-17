259a9cdf26a1b5917cdefda191b11c8fac04215c
Mizner_user <xmizner@stuba.sk>
2024-12-17 21:14:03 +0100

    options a readMe menu by uz malo fungovat

--- Core/Src/menu.c
+++ Core/Src/menu.c
@@ -83,15 +83,27 @@ void clearDifficulty() {
 	lcdPutSSized("HARD", 210, 144, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),3);
 }
 
-void menu(){
-	//int16_t difficulity = 2;		//v options sa bude dat menit
+drawReadme() {
+	lcdPutSSized("About game", 300, 40, decodeRgbValue(255, 255, 255), decodeRgbValue(0, 0, 0),2);
+	lcdPutSSized("STM adaptation of DOOM", 310, 80, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),1);
+	lcdPutSSized("press right button to escape", 310, 220, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),1);
+}
 
-	//difficulty();
-	//clearMenu();
+clearReadme() {
+	lcdPutSSized("About game", 300, 40, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+	lcdPutSSized("STM adaptation of DOOM", 310, 80, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),1);
+	lcdPutSSized("press right button to escape", 310, 220, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),1);
+}
+
+void menu(){
+	int16_t difficulity = 0;		//v options sa bude dat menit
+
+	clearMenu();
 	drawMenu();
 	LL_mDelay(3000);
 
 /*
+ 	//tieto premenne som sem daval, aby sa do nich dalo ulozit ak budeme chciet save game
 	int16_t ammo = 24;
 	int16_t health = 78;
 	int16_t armor = 0;
@@ -99,120 +111,158 @@ void menu(){
 	char healthText[16];
 	char armorText[16];
 */
-	//lowerMenu();
+//lowerMenu();
 
-		int select = 1;
+	int select = 1;			//tymto sa vybera iba v main menu
 
-		while(select != 6)	//main menu cycle
+	while(select != 6)	//main menu cycle
+	{
+
+		if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1) == GPIO_PIN_RESET)	//choose lower option
 		{
-
-			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1) == GPIO_PIN_RESET)	//choose lower option
-			{
-				if(select == 0)
-					select = 5;
-				else if(select != 0)
-					select--;
-			}
-
-			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0) == GPIO_PIN_RESET)	//choose option above
-			{
-				if(select == 5)
-					select = 0;
-				else if(select != 5)
-					select++;
-			}
-
 			if(select == 0)
-			{
-				lcdPutSSized("NEW GAME", 205, 64, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-				lcdPutSSized("NEW GAME", 205, 64, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-			}
-			if(select == 1)
-			{
-				lcdPutSSized("OPTIONS", 205, 80, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-				lcdPutSSized("OPTIONS", 205, 80, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+				select = 5;
+			else if(select != 0)
+				select--;
+		}
 
-				lcdPutS("AMMO", 305, 230, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
-				sprintf(ammoText, "%d", ammo);
-				lcdPutS(ammoText, 295, 222, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
-
-				lcdPutS("HEALTH", 160, 230, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
-				sprintf(healthText, "%d%%", health);
-				lcdPutS(healthText, 145, 222, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
-
-				lcdPutS("ARMOR", 35, 230, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
-				sprintf(armorText, "%d%%", armor);
-				lcdPutS(armorText, 20, 222, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
-			}
-			if(select == 2)
-			{
-				lcdPutSSized("LOAD GAME", 205, 96, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-				lcdPutSSized("LOAD GAME", 205, 96, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-			}
-			if(select == 3)
-			{
-				lcdPutSSized("SAVE GAME", 205, 112, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-				lcdPutSSized("SAVE GAME", 205, 112, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-			}
-			if(select == 4)
-			{
-				lcdPutSSized("READ THIS!", 205, 128, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-				lcdPutSSized("READ THIS!", 205, 128, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-			}
+		if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0) == GPIO_PIN_RESET)	//choose option above
+		{
 			if(select == 5)
-			{
-				lcdPutSSized("QUIT GAME", 205, 144, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-				lcdPutSSized("QUIT GAME", 205, 144, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-			}
+				select = 0;
+			else if(select != 5)
+				select++;
+		}
 
-			//confirm your option
-			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1) == GPIO_PIN_RESET && select == 0)
-				select = 6;
+		if(select == 0)
+		{
+			lcdPutSSized("NEW GAME", 205, 64, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+			lcdPutSSized("NEW GAME", 205, 64, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+		}
+		if(select == 1)
+		{
+			lcdPutSSized("OPTIONS", 205, 80, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+			lcdPutSSized("OPTIONS", 205, 80, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+
+			lcdPutS("AMMO", 305, 230, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
+			sprintf(ammoText, "%d", ammo);
+			lcdPutS(ammoText, 295, 222, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
+
+			lcdPutS("HEALTH", 160, 230, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
+			sprintf(healthText, "%d%%", health);
+			lcdPutS(healthText, 145, 222, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
+
+			lcdPutS("ARMOR", 35, 230, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
+			sprintf(armorText, "%d%%", armor);
+			lcdPutS(armorText, 20, 222, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0));
+		}
+		if(select == 2)
+		{
+			lcdPutSSized("LOAD GAME", 205, 96, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+			lcdPutSSized("LOAD GAME", 205, 96, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+		}
+		if(select == 3)
+		{
+			lcdPutSSized("SAVE GAME", 205, 112, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+			lcdPutSSized("SAVE GAME", 205, 112, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+		}
+		if(select == 4)
+		{
+			lcdPutSSized("READ THIS!", 205, 128, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+			lcdPutSSized("READ THIS!", 205, 128, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+		}
+		if(select == 5)
+		{
+			lcdPutSSized("QUIT GAME", 205, 144, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+			lcdPutSSized("QUIT GAME", 205, 144, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+		}
+
+		//zacat novu hru
+		if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET && select == 0)
+			select = 6;
 
 
-			// odkaz pre Mira: tak ako robil Matej menu, tak skus podobne urobit options a aj readme.
-			// V options sa bude prepinat obtiaznost a ked zacnes hru, tak sa hodnota obtiaznosti
-			// priradi hre. Obtiaznost => easy, medium, hard (0 - 1 - 2)
+		// odkaz pre Mira: tak ako robil Matej menu, tak skus podobne urobit options a aj readme.
+		// V options sa bude prepinat obtiaznost a ked zacnes hru, tak sa hodnota obtiaznosti
+		// priradi hre. Obtiaznost => easy, medium, hard (0 - 1 - 2)
 
-			// takto nejako si predstavujem options, skus to rozvinut
-			//confirm your option
-			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET && select == 1){
-				clearMenu();
-				//lcdPutSSized("options", 240, 0, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),4);
-				drawDifficulty();
-				while(select != 0){
-					if(select == 0) // tu namiesto select by asi davalo zmysel dat premennu difficulity ci to j eteraz len tak alebo ako ?
-					{
-						lcdPutSSized("EASY", 210, 80, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-						LL_mDelay(1000);
-						lcdPutSSized("EASY", 210, 80, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-					}
+		// takto nejako si predstavujem options, skus to rozvinut
+		//enter options menu
+		if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET && select == 1){
+			clearMenu();
+			drawDifficulty();
+			while(1){
 
-					if(select == 1)
-					{
-						lcdPutSSized("MEDIUM", 210, 112, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-						LL_mDelay(500);
-						lcdPutSSized("MEDIUM", 210, 112, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-					}
+				// vyber difficulity podla stlacenia tlacitok
+				if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1) == GPIO_PIN_RESET)	//choose lower option
+				{
+					if(difficulity == 0)
+						difficulity = 2;
+					else if(difficulity != 0)
+						difficulity--;
+				}
 
-					if(select == 2)
-					{
-						lcdPutSSized("HARD", 210, 144, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
-						LL_mDelay(750);
-						lcdPutSSized("HARD", 210, 144, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
-					}
-					if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET && select == 1){
-						//clearDifficulty();
-						drawMenu;
-						//lowerMenu;
-					}
+				if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0) == GPIO_PIN_RESET)	//choose option above
+				{
+					if(difficulity == 2)
+						difficulity = 0;
+					else if(difficulity != 2)
+						difficulity++;
+				}
+
+
+				if(difficulity == 0)
+				{
+					lcdPutSSized("EASY", 210, 80, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+					LL_mDelay(1000);
+					lcdPutSSized("EASY", 210, 80, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+				}
+
+				if(difficulity == 1)
+				{
+					lcdPutSSized("MEDIUM", 210, 112, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+					LL_mDelay(500);
+					lcdPutSSized("MEDIUM", 210, 112, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+				}
+
+				if(difficulity == 2)
+				{
+					lcdPutSSized("HARD", 210, 144, decodeRgbValue(0, 0, 0), decodeRgbValue(0, 0, 0),2);
+					LL_mDelay(750);
+					lcdPutSSized("HARD", 210, 144, decodeRgbValue(31, 31, 31), decodeRgbValue(0, 0, 0),2);
+				}
+
+				//ukoncit options menu potvrdenim vyberu, snad pin3 je spravny pin
+				if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET){
+					clearDifficulty();
+					drawMenu();
+					break;
 				}
 			}
+		}
+
+		//ked kliknes na "read this"
+		if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET && select == 4) {
+			clearMenu();
+			drawReadme();
+			while(1) {
+				if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3) == GPIO_PIN_RESET){
+					clearDifficulty();
+					drawMenu();
+					break;
+				}
+			}
+			clearReadme();
+			drawMenu();
+		}
+
+
 
 	clearMenu();
+	// myslim, ze uz sa da prepinat difficulity, takze hra sa bude dat spustit
 	//startNewGame(difficulity);
-		}
 	}
+}
 
 /*
  * po prejdeni z hlavneho menu na options menu mi tam vypise este save game read this quit neviem preco
